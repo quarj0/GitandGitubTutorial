121c836820f539bfa9c1b6202173f10f7415e1be
quarj0 <owusuansahkwadwo24@gmail.com>
2026-07-22 13:00:39 +0000

    add more files

---
+++ casting.py
@@ -0,0 +1,7 @@
+x = str(3)
+print("The data type of x is", type(x))
+y = 3
+print("The data type of y is", type(y))
+
+z = float(3)
+print("The data type of z is", type(z))
---
+++ check.py
@@ -0,0 +1,14 @@
+# txt = "The best things in life are free!"
+# print("no" in txt)
+
+
+# txt = "The best things in life are free!"
+# if "no" in txt:
+#     print("Yes, 'free' is present.")
+# else:
+#     print("No, this 'word' is absent.")
+
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
